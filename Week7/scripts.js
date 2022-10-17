1e670878277a8f674c2f66479a82b8b92e0cab66
zealotJ <jianj691@newschool.edu>
2022-10-17 17:28:01 -0400

    api

---
+++ Week7/scripts.js
@@ -0,0 +1,38 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e7b656032dmsh9ca648b2b2eb9f2p1512eajsnf330914cbada',
+		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+	}
+};
+
+const number = Math.floor(Math.random() * 500) + 1;
+
+// fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${number}`, options)
+//         .then(response => response.json())
+//         .then(response => console.log(response.id))
+//         .catch(err => console.error(err));
+
+async function getData(){
+    const response = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${number}`, options);
+    const data = await response.json();
+    console.log(data);
+    document.getElementById('name').innerHTML = data.title;
+    document.getElementById('img').src = data.thumbnail;
+    document.getElementById('date').textContent = data.release_date;
+    document.getElementById('platform').textContent = data.platform;
+    document.getElementById('genre').textContent = data.genre;
+    document.getElementById('dev').textContent = data.developer;
+    document.getElementById('btn').href = data.game_url;    
+}
+
+getData();
+
+
+
+//----------------------------------------blanks----------------------------------
+// function fillBlanks(){
+//     document.getElementById('name').innerHTML = response.id;
+// }
+
+// fillBlanks();
